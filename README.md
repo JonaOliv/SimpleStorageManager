4a71f4b930ce0501e6e447c9dc568918931ddb5b
Jonathan Oliva <10662970+JonaOliv@users.noreply.github.com>
2023-01-15 07:54:03 -0500

    navbar

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # SimpleStorageManager
+
+Simple structure project for the management of office resources in a department. IT should help as a fast template. Made with ReactJS.
+
+
+## Compiling and Running
+```
+npm install
+npm start
+```
--- src/components/NavigatorBar.js
+++ src/components/NavigatorBar.js
@@ -3,19 +3,19 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 import { Link } from 'react-router-dom';
-
+import './styles/navbar.css'
 
 function NavigatorBar() {
   return (
-    <Navbar bg="dark" expand="lg">
+    <Navbar expand="lg" className='JO-navbar'>
       <Container>
-        <Navbar.Brand as={Link} to="/home">Home</Navbar.Brand>
+        <Navbar.Brand className='JO-navbar-home' as={Link} to="/home">Home</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/employees">Employees</Nav.Link>
-            <Nav.Link as={Link} to="/inventory">Inventory</Nav.Link>
-            <Nav.Link as={Link} to="/records">Records</Nav.Link>
+            <Nav.Link className='JO-navbar-button' as={Link} to="/employees">Employees</Nav.Link>
+            <Nav.Link className='JO-navbar-button' as={Link} to="/inventory">Inventory</Nav.Link>
+            <Nav.Link className='JO-navbar-button' as={Link} to="/records">Records</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
